a6701b21f73b9f661fee7e33f54e8509a1ec769d
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2016-10-05 18:13:30 -0300

    subindo arquivos de build

--- build/javascripts/app/controller/estatisticas-334f311b.js
+++ build/javascripts/app/controller/estatisticas-334f311b.js
@@ -128,32 +128,7 @@
           }
         ]
       };
-      chartOptions = {
-        scales: {
-          xAxes: [
-            {
-              gridLines: {
-                display: false
-              }
-            }
-          ],
-          yAxes: [
-            {
-              display: false,
-              gridLines: {
-                display: false
-              }
-            }
-          ]
-        },
-        title: {
-          display: false
-        },
-        responsive: true,
-        legend: {
-          display: false
-        }
-      };
+      chartOptions = {};
       chartOptions.showTooltips = false;
       chartOptions.onAnimationComplete = function() {
         var ctx;
