c507f08be6ff65278e2e8f803a0935220ba17afd
hernani <hernani.luiz@gmail.com>
2018-04-03 17:26:26 -0300

    Add api to develop

--- config.rb
+++ config.rb
@@ -63,7 +63,7 @@ end
 # Build-specific configuration
 configure :build do
   # api host address
-  config[:api_host] = "/api"
+  config[:api_host] = "https://api-to.votolegal.com.br/api"
   #puts ("*" * 12) + "- MUDAR HOST DA API PARA PRODUÇÃO"
   #config[:api_host] = "//45.55.50.43/api"
 
