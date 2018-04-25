684be68a56e5cbe1995fbd414671227974acbdca
hernani <hernani.luiz@gmail.com>
2018-04-25 00:38:51 -0300

    Resolve conflict

--- build/javascripts/app/controller/default-a6262dc4.js
+++
@@ -1,8 +0,0 @@
-/**
- * Default Controller
- */
-
-app.votolegal.controller('DefaultController', ["$scope", "$http", "auth_service", "serialize", function($scope, $http, auth_service, serialize){
-  // validate user
-  auth_service.validate_user({role: 'user'});
-}]);
